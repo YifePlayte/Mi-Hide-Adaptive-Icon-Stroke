d563a73285baa32b765538b670e405dd1b4fcfe8
YifePlayte <62252171+YifePlayte@users.noreply.github.com>
2024-10-24 13:06:02 +0800

    Optimize dex injection

--- module/jni/module.h
+++ module/jni/module.h
@@ -1,13 +1,12 @@
 #pragma once
 
-namespace zygiskmodule {
+namespace mi_hide_adaptive_icon_stroke {
 
-static constexpr auto TAG = "MiHideAdaptiveIconStroke/JNI";
-
-static constexpr auto MODULE_DEX_PATH = "/data/adb/modules/mi_hide_adaptive_icon_stroke/classes.dex";
+    static constexpr auto TAG = "MiHideAdaptiveIconStroke/JNI";
+    static constexpr auto MODULE_DEX_PATH = "/data/adb/modules/mi_hide_adaptive_icon_stroke/classes.dex";
+    static constexpr auto ENTRY_CLASS_NAME = "com.yifeplayte.mihideadaptiveiconstroke.module.Main";
 
 #define LOGD(...)     __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
-
 #define LOGI(...)     __android_log_print(ANDROID_LOG_INFO,  TAG, __VA_ARGS__)
 #define LOGE(...)     __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
 #define LOGERRNO(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__ ": %d (%s)", errno, strerror(errno))
